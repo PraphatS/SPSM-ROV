68d91be4e9857f99f779ed6e31447a140408aedf
PraphatS <131101422+PraphatS@users.noreply.github.com>
2025-07-31 10:44:32 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,48 @@
-function openPopup(videoUrl) {
-  const modal = document.getElementById("videoModal");
-  const iframe = document.getElementById("videoFrame");
-  iframe.src = videoUrl;
-  modal.style.display = "block";
+const videoData = {
+  match1: [
+    "https://www.youtube.com/embed/dQw4w9WgXcQ",
+    "https://www.youtube.com/embed/ScMzIvxBSi4"
+  ],
+  match2: [
+    "https://www.youtube.com/embed/tgbNymZ7vqY",
+    "https://www.youtube.com/embed/dQw4w9WgXcQ"
+  ],
+  match3: [
+    "https://www.youtube.com/embed/ScMzIvxBSi4"
+  ],
+  match4: [
+    "https://www.youtube.com/embed/tgbNymZ7vqY"
+  ],
+  match5: [
+    "https://www.youtube.com/embed/dQw4w9WgXcQ",
+    "https://www.youtube.com/embed/ScMzIvxBSi4",
+    "https://www.youtube.com/embed/tgbNymZ7vqY"
+  ],
+  match6: [
+    "https://www.youtube.com/embed/ScMzIvxBSi4"
+  ],
+  match7: [
+    "https://www.youtube.com/embed/dQw4w9WgXcQ"
+  ],
+  match8: [
+    "https://www.youtube.com/embed/tgbNymZ7vqY"
+  ]
+};
+
+function openPopup(matchKey) {
+  const container = document.getElementById("videoContainer");
+  container.innerHTML = "";
+
+  videoData[matchKey].forEach(url => {
+    const iframe = document.createElement("iframe");
+    iframe.src = url;
+    container.appendChild(iframe);
+  });
+
+  document.getElementById("videoModal").style.display = "block";
 }
 
 function closePopup() {
-  const modal = document.getElementById("videoModal");
-  const iframe = document.getElementById("videoFrame");
-  iframe.src = "";
-  modal.style.display = "none";
+  document.getElementById("videoModal").style.display = "none";
+  document.getElementById("videoContainer").innerHTML = "";
 }
